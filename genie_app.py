0e9b54bd53e4ea98c597724336207ed3151439e1
LucyZi <64695107+LucyZi@users.noreply.github.com>
2026-01-29 21:14:27 +0800

    Update genie_app.py

--- genie_app.py
+++ genie_app.py
@@ -192,6 +192,7 @@ HTML_TEMPLATE = """
                             beginAtZero: false,
                             ticks: {
                                 callback: function(value) {
+                                    if (Math.abs(value) >= 1e9) return (value / 1e9).toFixed(2) + 'B';
                                     if (Math.abs(value) >= 1e6) return (value / 1e6).toFixed(2) + 'M';
                                     if (Math.abs(value) >= 1e3) return (value / 1e3).toFixed(2) + 'K';
                                     return value;
